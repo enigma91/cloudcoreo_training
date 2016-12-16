e0adb4ec060d7f5b132e53e1833f80155e37075a
Natalia <natalia@cloudcoreo.com>
2016-12-16 16:08:49 +0600

    init alert

--- services/config.rb
+++ services/config.rb
@@ -1,9 +1,9 @@
-coreo_aws_advisor_alert "elb-inventory" do
+coreo_aws_advisor_alert "elb-training-check" do
   action :define
   service :elb
   #include_violations_in_count false
   display_name "ELB Object Health Check"
-  description "Load balancer health check is greater then 2 min."
+  description "Load balancer health check is greater then 1 sec."
   category "Inventory"
   suggested_action "None."
   level "Informational"
@@ -13,8 +13,23 @@ coreo_aws_advisor_alert "elb-inventory" do
   alert_when [1]
 end
 
+coreo_aws_advisor_alert "ec2-training-check" do
+  action :define
+  service :ec2
+  #include_violations_in_count false
+  display_name "Ec2 large not encrypted snapshots"
+  description "EC2 not encrypted snapshot is greater than 256GB"
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["snapshots"]
+  audit_objects ["snapshot_set.volume_size", "snapshot_set.encrypted"]
+  operators [">", "="]
+  alert_when [256, false]
+end
+
 coreo_aws_advisor_elb "advise-elb" do
-  alerts ${AUDIT_AWS_ELB_ALERT_LIST}
+  alerts ["elb-training-check", "ec2-training-check"]
   action :advise
   regions ${AUDIT_AWS_ELB_REGIONS}
 end
